770ec31a6ff9f8962d6bd7aeaf986e3ec4e73880
Sofyan Egi <sofyan.egi@bts.id>
2025-01-13 14:34:39 +0700

    feat(page): add home, contact, and about page

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', ["title" => "Home Page"]);
+});
+Route::get('/about', function () {
+    return view('about', ["title" => "About Page"]);
+});
+
+Route::get('/contact', function () {
+    return view('contact', ["title" => "Contact Page"]);
 });
