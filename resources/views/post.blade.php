d998fea55adfc2614e512a4e36be329c2ae8ee94
Sofyan Egi <sofyan.egi@bts.id>
2025-01-14 11:27:30 +0700

    style(post): redesign using flowbite

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,13 +1,45 @@
 <x-layout>
     <x-slot:title>{{ $title }}</x-slot:title>
 
-    <article class="py-8 max-w-screen-md border-gray-300">
-        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post->title }}</h2>
-        <div class="text-base text-gray-500">
-            <a href="/authors/{{ $post->user->username }}" class="hover:underline">{{ $post->user->name }}</a> |
-            {{ $post->created_at->diffForHumans() }}
-        </div>
-        <p class="mt-4 text-lg text-gray-600">{{ $post->body }}</p>
-        <a href="/posts/" class="font-medium text-blue-500 hover:underline"> &laquo; Back to posts</a>
-    </article>
+    <div class="flex justify-between px-4 mx-auto max-w-screen-xl ">
+        <article
+            class="mx-auto w-full max-w-4xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
+            <header class="mb-4 lg:mb-6 not-format">
+                <a href="/posts/" class="font-medium text-blue-500 hover:underline"> &laquo; Back to posts</a>
+                <address class="flex items-center mb-6 not-italic">
+                    <div class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
+                        <img class="mr-4 w-16 h-16 rounded-full"
+                            src="https://flowbite.com/docs/images/people/profile-picture-2.jpg" alt="Jese Leos">
+                        <div>
+                            <a href="/authors/{{ $post->user->username }}" rel="author"
+                                class="text-xl font-bold text-gray-900 dark:text-white">{{ $post->user->name }}</a>
+
+                            <p class="text-base text-gray-500 dark:text-gray-400"><time pubdate datetime="2022-02-08"
+                                    title="February 8th, 2022">{{ $post->created_at->diffForHumans() }}</time>
+                            </p>
+                            <span class="inline text-sm text-gray-500 dark:text-gray-400">
+                                <a href="/categories/{{ $post->category->slug }}"
+                                    class="inline-flex items-center px-2.5 py-0.5 text-xs font-medium text-gray-800 bg-gray-100 rounded dark:bg-gray-800 dark:text-gray-100">
+                                    <svg class="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <path
+                                            d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z">
+                                        </path>
+                                    </svg>
+                                    {{ $post->category->name }}
+                                </a>
+                            </span>
+                        </div>
+                    </div>
+                </address>
+                <h1
+                    class="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">
+                    {{ $post->title }}</h1>
+            </header>
+            <div class="not-format">
+                {!! $post->body !!}
+            </div>
+
+        </article>
+    </div>
 </x-layout>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,21 +1,54 @@
 <x-layout>
     <x-slot:title>{{ $title }}</x-slot:title>
 
-    @foreach ($posts as $post)
-        <article class="py-8 max-w-screen-md border-b border-gray-300">
-            <a href="/posts/{{ $post->slug }}">
-                <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post->title }}</h2>
-            </a>
-            <div class="text-base text-gray-500">
-                <a class="hover:underline" href="/authors/{{ $post->user->username }}">{{ $post->user->name }}</a> |
-                {{ $post->created_at->format('F j, Y') }}
-                <span>
-                    <a href="/categories/{{ $post->category->slug }}"
-                        class="px-3 py-1 bg-blue-200 text-blue-800 rounded-full hover:bg-blue-400">{{ $post->category->name }}</a>
-                </span>
-            </div>
-            <p class="mt-4 text-lg text-gray-600">{{ Str::limit($post->body, 100, '...') }}</p>
-            <a href="/posts/{{ $post->slug }}" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
-        </article>
-    @endforeach
+    <div class="py-4 px-4 mx-auto max-w-screen-xl lg:py-8 lg:px-0">
+        <div class="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
+            @foreach ($posts as $post)
+                <article
+                    class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+                    <div class="flex justify-between items-center mb-5 text-gray-500">
+                        <a href="/categories/{{ $post->category->slug }}">
+                            <span
+                                class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
+                                <svg class="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z">
+                                    </path>
+                                </svg>
+                                {{ $post->category->name }}
+                            </span>
+                        </a>
+                        <span class="text-sm"> {{ $post->created_at->format('F j, Y') }}</span>
+                    </div>
+                    <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                        <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
+                    </h2>
+                    <p class="mb-5 font-light text-gray-500 dark:text-gray-400">
+                        {{ Str::limit($post->body, 100, '...') }}
+                    </p>
+                    <div class="flex justify-between items-center">
+                        <div class="flex items-center space-x-4">
+                            <img class="w-7 h-7 rounded-full"
+                                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png"
+                                alt="Jese Leos avatar" />
+                            <span class="font-medium dark:text-white">
+                                <a href="/authors/{{ $post->user->username }}">{{ $post->user->name }}</a>
+                            </span>
+                        </div>
+                        <a href="/posts/{{ $post->slug }}"
+                            class="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
+                            Read more
+                            <svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd"
+                                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                    clip-rule="evenodd"></path>
+                            </svg>
+                        </a>
+                    </div>
+                </article>
+            @endforeach
+        </div>
+    </div>
 </x-layout>
